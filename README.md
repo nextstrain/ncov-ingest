c733ceb0784e11f113c413f3c665fef28f168e66
Kairsten Fay <kairsten.fay@gmail.com>
2020-02-11 15:46:16 -0800

    Create initial fetch/transform pipeline
    Add two scripts, one for fetching data from GISAID and
    one for transforming the fetched GISAID data into our
    desired `metadata.tsv` and `sequences.fasta` files.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# nCoV Ingest
+# nCoV Ingestion Pipeline
+
+## Required environment variables
+* `GISAID_API_ENDPOINT`
+* `GISAID_USERNAME_AND_PASSWORD`
+* `AWS_DEFAULT_REGION`
+* `AWS_ACCESS_KEY_ID`
+* `AWS_SECRET_ACCESS_KEY`
+
+Steps:
+1. run `./bin/fetch-data`
+2. run `python ./bin/transform-data.py {ncov json data}`
---
+++ bin/transform-data.py
@@ -0,0 +1,99 @@
+"""
+Parse the GISAID JSON load into a metadata tsv and a FASTA file.
+"""
+import argparse
+import json
+import pandas as pd
+from pathlib import Path
+
+# Note: 'sequence' should NEVER appear in this list!
+METADATA_COLUMNS = [  # Ordering of columns in the existing metadata.tsv in the ncov repo
+    'strain', 'virus', 'gisaid_epi_isl', 'genbank_accession', 'date', 'region',
+    'country', 'division', 'location', 'segment', 'host', 'originating_lab',
+    'submitting_lab', 'authors', 'url', 'title'
+]
+
+def parse_geographic_columns(gisaid_data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Expands the string found in the column named `covv_location` in the given
+    *df*, creating four new columns. Returns the modified ``pd.DataFrame``.
+    """
+    geographic_data = gisaid_data['covv_location'].str.split('/', expand=True)
+
+    gisaid_data['region']      = geographic_data[0]
+    gisaid_data['country']     = geographic_data[1]
+    gisaid_data['division']    = geographic_data[2]
+    gisaid_data['location']    = geographic_data[3]
+
+    return gisaid_data
+
+def generate_hardcoded_metadata(hardcoded_metadata: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns a ``pd.DataFrame`` with a column for strain ID plus additional
+    columns containing harcoded metadata.
+    """
+    hardcoded_metadata = pd.DataFrame(gisaid_data['strain'])
+    hardcoded_metadata['virus']             = 'ncov'
+    hardcoded_metadata['genbank_accession'] = '?'
+    hardcoded_metadata['url']               = 'https://www.gisaid.org'
+    # TODO verify these are all actually true
+    hardcoded_metadata['segment']           = 'genome'
+    hardcoded_metadata['title']             = 'Newly discovered betacoronavirus, 2019-2020'
+
+    return hardcoded_metadata
+
+def write_fasta_file(sequence_data: pd.DataFrame):
+    """ """
+    sequence_data['sequence'] = sequence_data['sequence'].str.replace('\n', '')
+
+    with open(args.output_fasta, 'wt') as fastafile:
+        for index, row in sequence_data.iterrows():
+            fastafile.write(f">{row['strain']}\n")
+            fastafile.write(f"{row['sequence']}\n\n")
+
+
+if __name__ == '__main__':
+    base = Path(__file__).resolve().parent.parent
+
+    parser = argparse.ArgumentParser(
+        description="Parse a GISAID JSON load into a metadata tsv and FASTA file.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("gisaid_data", help="Newline-delimited GISAID JSON data")
+    parser.add_argument("--metadata", help="Optional manually curated metadata tsv")
+    parser.add_argument("--output-metadata",
+        default=base / "data/metadata.tsv",
+        help="Output location of generated metadata tsv. Defaults to `data/metadata.tsv`")
+    parser.add_argument("--output-fasta",
+        default=base / "data/sequences.fasta",
+        help="Output location of generated FASTA file. Defaults to `data/sequences.fasta`")
+    args = parser.parse_args()
+
+    gisaid_data = pd.read_json(args.gisaid_data, lines=True)
+
+    mapper = {
+        'covv_virus_name'   : 'strain',
+        'covv_accession_id' : 'gisaid_epi_isl',
+        'covv_subm_date'    : 'date',
+        'covv_host'         : 'host',
+        'covv_orig_lab'     : 'originating_lab',
+        'covv_subm_lab'     : 'submitting_lab',
+        'covv_authors'      : 'authors',
+    }
+    gisaid_data.rename(mapper, axis="columns", inplace=True)
+
+    write_fasta_file(gisaid_data)
+
+    gisaid_data = parse_geographic_columns(gisaid_data)
+
+    if not args.metadata:
+        curated_metadata = generate_hardcoded_metadata(gisaid_data)
+    else:
+        curated_metadata = pd.read_csv(args.metadata, sep="\t")
+
+    curated_gisaid_data = gisaid_data.merge(curated_metadata)
+
+
+    # Reorder columns consistent with the existing metadata on GitHub
+    curated_gisaid_data = curated_gisaid_data[METADATA_COLUMNS]
+    curated_gisaid_data.to_csv(args.output_metadata, sep='\t', index=False)
