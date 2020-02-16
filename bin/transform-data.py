7c4baadc0eeeeac38f60226f0548f0ffaaec8b19
Trevor Bedford <trevor@bedford.io>
2020-02-15 19:20:57 -0800

    Update parsing
    1. 'date' should be 'covv_collection_date' and not 'covv_subm_date'
    2. Sort metadata and sequences based on 'strain' to keep updates cleaner
    3. Remove extraneous whitespace surrounding geo labels
    4. Replace 'title' with '?' (GISAID entries don't have title)
    5. Remove extraneous line return in FASTA output
    6. Switch --metadata to --annotations. I wanted to reserve "metadata.tsv" for
    the row/column dataframe. Annotations felt more semantically appropriate.
    7. Start an annotations file at `source-data/annotations.tsv` and default the
    transform-data script to use this file
    8. Use '?' for NA fields in metadata TSV export

--- bin/transform-data.py
+++ bin/transform-data.py
@@ -23,13 +23,13 @@ def preprocess(gisaid_data: pd.DataFrame) -> pd.DataFrame:
     DataFrame, returning the modified DataFrame.
     """
     mapper = {
-        'covv_virus_name'   : 'strain',
-        'covv_accession_id' : 'gisaid_epi_isl',
-        'covv_subm_date'    : 'date',
-        'covv_host'         : 'host',
-        'covv_orig_lab'     : 'originating_lab',
-        'covv_subm_lab'     : 'submitting_lab',
-        'covv_authors'      : 'authors',
+        'covv_virus_name'       : 'strain',
+        'covv_accession_id'     : 'gisaid_epi_isl',
+        'covv_collection_date'  : 'date',
+        'covv_host'             : 'host',
+        'covv_orig_lab'         : 'originating_lab',
+        'covv_subm_lab'         : 'submitting_lab',
+        'covv_authors'          : 'authors'
     }
     gisaid_data.rename(mapper, axis="columns", inplace=True)
 
@@ -38,14 +38,20 @@ def preprocess(gisaid_data: pd.DataFrame) -> pd.DataFrame:
         .str.replace(r'^BetaCoV/', '', n=1, case=False) \
         .str.replace(r'\s', '')
 
-    return gisaid_data.drop_duplicates(subset='strain')
+    # Dropping duplicates
+    gisaid_data.drop_duplicates(subset='strain', inplace=True)
+
+    # Sorting by strain name
+    gisaid_data.sort_values(by=['strain'], inplace=True)
+
+    return gisaid_data
 
 def parse_geographic_columns(gisaid_data: pd.DataFrame) -> pd.DataFrame:
     """
     Expands the string found in the column named `covv_location` in the given
     *df*, creating four new columns. Returns the modified ``pd.DataFrame``.
     """
-    geographic_data = gisaid_data['covv_location'].str.split('/', expand=True)
+    geographic_data = gisaid_data['covv_location'].str.split('\s*/\s*', expand=True)
 
     gisaid_data['region']      = geographic_data[0]
     gisaid_data['country']     = geographic_data[1]
@@ -65,7 +71,7 @@ def generate_hardcoded_metadata(hardcoded_metadata: pd.DataFrame) -> pd.DataFram
     hardcoded_metadata['url']               = 'https://www.gisaid.org'
     # TODO verify these are all actually true
     hardcoded_metadata['segment']           = 'genome'
-    hardcoded_metadata['title']             = 'Newly discovered betacoronavirus, 2019-2020'
+    hardcoded_metadata['title']             = '?'
 
     return hardcoded_metadata
 
@@ -76,7 +82,7 @@ def write_fasta_file(sequence_data: pd.DataFrame):
     with fsspec.open(args.output_fasta, 'wt') as fastafile:
         for index, row in sequence_data.iterrows():
             fastafile.write(f">{row['strain']}\n")
-            fastafile.write(f"{row['sequence']}\n\n")
+            fastafile.write(f"{row['sequence']}\n")
 
 def update_metadata(curated_gisaid_data: pd.DataFrame) -> pd.DataFrame:
     """ """
@@ -85,10 +91,10 @@ def update_metadata(curated_gisaid_data: pd.DataFrame) -> pd.DataFrame:
     curated_gisaid_data.update(hardcoded_metadata)
     curated_gisaid_data = curated_gisaid_data.merge(hardcoded_metadata)
 
-    if args.metadata:
-        # Use the curated metadata tsv to update any column values
-        user_provided_metadata = pd.read_csv(args.metadata, header=None, sep='\t')
-        for index, row in user_provided_metadata.iterrows():
+    if args.annotations:
+        # Use the curated annotations tsv to update any column values
+        user_provided_annotations = pd.read_csv(args.annotations, header=None, sep='\t')
+        for index, row in user_provided_annotations.iterrows():
             curated_gisaid_data.loc[curated_gisaid_data['strain'] == row[0], row[1]] = row[2]
 
     return curated_gisaid_data
@@ -102,14 +108,15 @@ if __name__ == '__main__':
         formatter_class=argparse.RawTextHelpFormatter
     )
     parser.add_argument("gisaid_data", help="Newline-delimited GISAID JSON data")
-    parser.add_argument("--metadata",
-        help="Optional manually curated metadata TSV.\n"
+    parser.add_argument("--annotations",
+        default=base / "source-data/annotations.tsv",
+        help="Optional manually curated annotations TSV.\n"
             "The TSV file should have no header and exactly three columns which contain:\n\t"
             "1. the strain ID\n\t"
             "2. the column name to replace from the generated `metadata.tsv` file\n\t"
             "3. the replacement data\n"
         "e.g.\n\t"
-        "USA-MA1/2020    location    Boston\n\t"
+        "USA/MA1/2020    location    Boston\n\t"
         "USA/CA1/2020    genbank_accession   MN994467\n\t"
         "Wuhan-Hu-1/2019 collection_date 2019-12-26")
     parser.add_argument("--output-metadata",
@@ -132,4 +139,4 @@ if __name__ == '__main__':
 
     # Reorder columns consistent with the existing metadata on GitHub
     curated_gisaid_data = curated_gisaid_data[METADATA_COLUMNS]
-    curated_gisaid_data.to_csv(args.output_metadata, sep='\t', index=False)
+    curated_gisaid_data.to_csv(args.output_metadata, sep='\t', na_rep='?', index=False)
