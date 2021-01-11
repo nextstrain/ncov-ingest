af0c948148069322064b069811d2597df87c867a
MoiraZuber <moira.zuber@stud.unibas.ch>
2021-01-11 17:31:56 +0100

    add purpose_of_sequencing to METADATA_COLUMNS

--- lib/utils/transform.py
+++ lib/utils/transform.py
@@ -11,7 +11,7 @@ METADATA_COLUMNS = [  # Ordering of columns in the existing metadata.tsv in the 
     'country', 'division', 'location', 'region_exposure', 'country_exposure',
     'division_exposure', 'segment', 'length', 'host', 'age', 'sex', 'pangolin_lineage', 'GISAID_clade',
     'originating_lab', 'submitting_lab', 'authors', 'url', 'title', 'paper_url',
-    'date_submitted'
+    'date_submitted', 'purpose_of_sequencing'
 ]
 
 
