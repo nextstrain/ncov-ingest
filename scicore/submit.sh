5d4b3294f89e5062ece466ea3de13fc906f602bb
Richard Neher <richard.neher@unibas.ch>
2021-01-17 22:27:03 +0100

    fixes for cluster

--- scicore/submit.sh
+++ scicore/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#SBATCH --output=log/%j.out                 # where to store the output ( %j is the JOBID )
-#SBATCH --error=log/%j.err                  # where to store error messages
+#SBATCH --output=cluster_logs/%j.out                 # where to store the output ( %j is the JOBID )
+#SBATCH --error=cluster_logs/%j.err                  # where to store error messages
 
 # activate conda environment
 conda activate nextstrain
